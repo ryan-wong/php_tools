e81dcdccc9c6d67eee45fd203a166398f4a4d352
ryan-wong <wongryan2001@gmail.com>
2013-03-04 13:21:06 -0500

    working alter table

--- schema.php
+++ schema.php
@@ -6,6 +6,7 @@ class DatabaseSchema {
     protected $_tableNames = array();
     protected $_tables = array();
     protected $_con = null;
+    protected $_schema = '';
 
     public function __construct($settings) {
         $this->_databaseName = $settings[3];
@@ -42,35 +43,106 @@ class DatabaseSchema {
                     foreach ($fields as $field) {
                         $tableArray[$field] = $row[$field];
                     }
-                    $this->_tables[$table][] = $tableArray;
+                    $this->_tables[$table][$row['COLUMN_NAME']] = $tableArray;
                 }
             }
         }
         return $this->_tables;
     }
 
-    public function compareDatabase(DatabaseSchema $schema2) {
-        $tableDifferences = array_diff_assoc($this->getTableNames(), $schema2->getTableNames());
-		$schema1Str = '';
-		$schema2Str = '';
-        foreach ($tableDifferences as $diff) {
-            if (in_array($diff, $this->getTableNames())) {
-                //echo "Table $diff is in Database Schema 1<br/>";
-                $schema1Str .= DatabaseSchema::createTable($this->getTables()[$diff],$diff);
-            } else {
-                //echo "Table $diff is in Database Schema 2<br/>";
-                $schema2Str.= DatabaseSchema::createTable($schema2->getTables()[$diff],$diff);
+    public function my_array_diff_assoc($a, $a2) {
+        $diff = array();
+        foreach ($a as $table) {
+            if (!in_array($table, $a2)) {
+                $diff[] = $table;
             }
         }
-      $schemaStr = $schema1Str."\n-------------------\n".$schema2Str;
-      $fileName = "schema.sql";  
-      header("Content-type: application/force-download");
-      header("Content-Disposition: attachment; filename=$fileName;size=" . strlen($schemaStr));
-      echo $schemaStr;
-      exit;
+        foreach ($a2 as $table) {
+            if (!in_array($table, $a)) {
+                $diff[] = $table;
+            }
+        }
+        return $diff;
     }
 
-    public static function createTable($tableArray,$table) {
+    public function compareDatabase(DatabaseSchema $schema2) {
+        $tableDifferences = $this->my_array_diff_assoc($this->getTableNames(), $schema2->getTableNames());
+        $schema1Str = '';
+        $schema2Str = '';
+        foreach ($tableDifferences as $diff) {
+            if (in_array($diff, $this->getTableNames())) {
+                $schema1Str .= DatabaseSchema::createTable($this->getTables()[$diff], $diff);
+            } else {
+                $schema2Str.= DatabaseSchema::createTable($schema2->getTables()[$diff], $diff);
+            }
+        }
+        $schema1Table = $this->getTables();
+        $schema2Table = $schema2->getTables();
+        $ignoreField = array('TABLE_SCHEMA', 'PRIVILEGES', 'ORDINAL_POSITION', 'TABLE_CATALOG');
+        foreach ($schema1Table as $tableName => $tableArray) {
+            foreach ($tableArray as $field => $fieldArray) {
+                foreach ($fieldArray as $attribute => $value) {
+                    if ($value != $schema2Table[$tableName][$field][$attribute] && !in_array($attribute, $ignoreField)) {
+                        $schema2Str .= DatabaseSchema::alterTable($fieldArray, $tableName);
+                        break;
+                    }
+                }
+            }
+        }
+        $schemaStr = "Schema 1:" . $schema1Str . "\n-------------------------------------------------------------------------------------------------------\nSchema 2: \n" . $schema2Str;
+        $this->_schema = $schemaStr;
+        echo str_replace("\n", '<br/>', $schemaStr);
+    }
+
+    public function forceSqlDownload() {
+        $schemaStr = $this->_schema;
+        $fileName = "schema.sql";
+        header("Content-type: application/force-download");
+        header("Content-Disposition: attachment; filename=$fileName;size=" . strlen($schemaStr));
+        echo $schemaStr;
+        exit;
+    }
+
+    public static function alterTable($column, $table) {
+        $primaryKey = '';
+        $uniqueKey = '';
+        $columnName = $column['COLUMN_NAME'];
+        $columnType = $column['COLUMN_TYPE'];
+        $columnIsNull = ($column['IS_NULLABLE'] == 'NO') ? 'NOT NULL' : 'NULL';
+        $columnExtra = ($column['EXTRA'] == 'auto_increment') ? 'AUTO_INCREMENT' : '';
+        $columnDefault = '';
+        if ($column['COLUMN_DEFAULT']) {
+            if ($column['COLUMN_DEFAULT'] == 'CURRENT_TIMESTAMP') {
+                $columnDefault = "DEFAULT " . $column['COLUMN_DEFAULT'];
+            } else {
+                $columnDefault = "DEFAULT '" . $column['COLUMN_DEFAULT'] . "'";
+            }
+        }
+        $columnComment = '';
+        $columnCharacter = '';
+        if ($column['COLLATION_NAME']) {
+            $columnCharacter = "CHARACTER SET " . $column['CHARACTER_SET_NAME'] . " COLLATE " . $column['COLLATION_NAME'];
+        }
+        if ($column['COLUMN_COMMENT']) {
+            $columnComment = "COMMENT '" . $column['COLUMN_COMMENT'] . "'";
+        }
+        if ($column['COLUMN_KEY'] == 'PRI') {
+            $primaryKey = "ALTER TABLE `$table` DROP PRIMARY KEY, ADD PRIMARY KEY(`$columnName`);\n";
+        }
+        if ($column['COLUMN_KEY'] == 'UNI') {
+            $uniqueKey = "ALTER TABLE `$table` ADD UNIQUE( `$columnName`);\n";
+        }
+        $alterStr = trim("ALTER TABLE `$table` CHANGE `$columnName` `$columnName` $columnType $columnCharacter $columnIsNull $columnDefault $columnExtra  $columnComment") . ";\n";
+        if ($primaryKey) {
+            $alterStr .= $primaryKey;
+        }
+        if ($uniqueKey) {
+            $alterStr.= $uniqueKey;
+        }
+        return $alterStr;
+    }
+
+    public static function createTable($tableArray, $table) {
         $tableStr = "CREATE TABLE IF NOT EXISTS `$table` (\n ";
         $primaryKey = '';
         $uniqueKeys = array();
@@ -85,7 +157,7 @@ class DatabaseSchema {
                 if ($column['COLUMN_DEFAULT'] == 'CURRENT_TIMESTAMP') {
                     $columnDefault = "DEFAULT " . $column['COLUMN_DEFAULT'];
                 } else {
-                    $columnDefault = "DEFAULT '" . $column['COLUMN_DEFAULT']."'";
+                    $columnDefault = "DEFAULT '" . $column['COLUMN_DEFAULT'] . "'";
                 }
             }
             $columnComment = '';
@@ -94,7 +166,7 @@ class DatabaseSchema {
                 $hasAutoIncrement = true;
             }
             if ($column['COLLATION_NAME']) {
-                $columnCharacter = "CHARACTER SET ".$column['CHARACTER_SET_NAME']." COLLATE " . $column['COLLATION_NAME'];
+                $columnCharacter = "CHARACTER SET " . $column['CHARACTER_SET_NAME'] . " COLLATE " . $column['COLLATION_NAME'];
             }
             if ($column['COLUMN_COMMENT']) {
                 $columnComment = "COMMENT '" . $column['COLUMN_COMMENT'] . "'";
@@ -105,12 +177,12 @@ class DatabaseSchema {
             if ($column['COLUMN_KEY'] == 'UNI') {
                 $uniqueKeys[] = "UNIQUE KEY `$columnName` (`$columnName`)";
             }
-            $columnStr = trim("`$columnName` $columnType $columnCharacter $columnIsNull $columnDefault $columnExtra  $columnComment").",\n";
+            $columnStr = trim("`$columnName` $columnType $columnCharacter $columnIsNull $columnDefault $columnExtra  $columnComment") . ",\n";
             $tableStr .= $columnStr;
         }
         $tableStr .= $primaryKey;
-        if($uniqueKeys){
-        $tableStr .= ",\n";
+        if ($uniqueKeys) {
+            $tableStr .= ",\n";
         }
         $tableStr .= implode(",\n", $uniqueKeys);
         $tableStr .="\n)ENGINE=MyISAM DEFAULT CHARSET=utf8 ";
@@ -120,32 +192,6 @@ class DatabaseSchema {
             $tableStr .= ";\n";
         }
         return $tableStr;
-//        CREATE TABLE IF NOT EXISTS `testsss` (
-//  `id` int(12) NOT NULL AUTO_INCREMENT COMMENT 'sdfsdfsd',
-//  `erw` varchar(123) CHARACTER SET big5 COLLATE big5_bin DEFAULT NULL COMMENT 'sxxxxxxxdfsdffdfdd',
-//  `sffsdf` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'cxcxvxv',
-//  PRIMARY KEY (`id`),
-//  UNIQUE KEY `erw` (`erw`)
-//) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='testttttttt' AUTO_INCREMENT=1 ;
-// 'TABLE_CATALOG' => string 'def' (length=3)
-//          'TABLE_SCHEMA' => string 'development' (length=11)
-//          'TABLE_NAME' => string 'match' (length=5)
-//          'COLUMN_NAME' => string 'id' (length=2)
-//          'ORDINAL_POSITION' => string '1' (length=1)
-//          'COLUMN_DEFAULT' => null
-//          'IS_NULLABLE' => string 'NO' (length=2)
-//          'DATA_TYPE' => string 'int' (length=3)
-//          'CHARACTER_MAXIMUM_LENGTH' => null
-//          'CHARACTER_OCTET_LENGTH' => null
-//          'NUMERIC_PRECISION' => string '10' (length=2)
-//          'NUMERIC_SCALE' => string '0' (length=1)
-//          'CHARACTER_SET_NAME' => null
-//          'COLLATION_NAME' => null
-//          'COLUMN_TYPE' => string 'int(11)' (length=7)
-//          'COLUMN_KEY' => string 'PRI' (length=3)
-//          'EXTRA' => string 'auto_increment' (length=14)
-//          'PRIVILEGES' => string 'select,insert,update,references' (length=31)
-//          'COLUMN_COMMENT' => string '' (length=0)
     }
 
 }
